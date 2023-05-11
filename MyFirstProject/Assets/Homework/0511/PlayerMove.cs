ea187b88ff7cce248698ef2e964802bbb662d7ce
AN YE SEUL <as5948@naver.com>
2023-05-11 21:50:33 +0900

    feat : temp commit

--- MyFirstProject/Assets/Homework/0511/PlayerMove.cs
+++ MyFirstProject/Assets/Homework/0511/PlayerMove.cs
@@ -10,16 +10,17 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log($"[Start] : {Player.name}");
+        Debug.Log($"[Before] {Player.name}");
         Player.name = "Change_Player";
+        Debug.Log($"[After] {Player.name}");
 
-        Player.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        Player.AddForce(Vector3.up * Power, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"[Update] : {Player.name}");
+       // Debug.Log($"[Update] : {Player.name}");
 
     }
 }
