15c4bf648089c8dea7f74453e3c1f411dcce94d0
YS AN <as5948@naver.com>
2023-08-23 21:45:45 +0900

    feat : (temp commit) study interactable

--- VRBasic/Assets/Scripts/InteractorController.cs
+++ VRBasic/Assets/Scripts/InteractorController.cs
@@ -3,104 +3,136 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.UI;
 using static UnityEngine.InputSystem.InputAction;
 
 public class InteractorController : MonoBehaviour
 {
-    [SerializeField] XRRayInteractor rayInteractor;
-    [SerializeField] XRRayInteractor teleportInteractor;
+	[SerializeField]
+	private XRDirectInteractor directInteractor;
 
-    [SerializeField] List<LocomotionProvider> locomotions;
+	[SerializeField]
+	private XRRayInteractor rayInteractor;
 
-    [SerializeField] InputActionReference teleportModeActivate;
+	[SerializeField]
+	private XRRayInteractor teleportInteractor;
 
-    private void Start()
-    {
-        if (rayInteractor != null)
-        {
-            rayInteractor.gameObject.SetActive(true);
-        }
-        if (teleportInteractor != null)
-        {
-            teleportInteractor.gameObject.SetActive(false);
-        }
-    }
 
-    private void OnEnable()
-    {
-        if (rayInteractor != null)
-        {
-            rayInteractor.selectEntered.AddListener(DisableLocomotions);
-            rayInteractor.selectExited.AddListener(EnableLocomotions);
-        }
+	[SerializeField]
+	private InputActionReference teleportInput; //teleport 키를 눌렀는지 확인하기 위한 변수
+													   //InputActionReference : inputAction 안에 있는 행동을 지정해서 사용이 가능함
 
-        InputAction teleportModeActivate = this.teleportModeActivate?.action;
-        if (teleportModeActivate != null)
-        {
-            teleportModeActivate.performed += OnStartTeleport;
-            teleportModeActivate.canceled += OnStopTeleport;
-        }
-    }
+	[SerializeField]
+	private List<LocomotionProvider> locomotionProviders;
 
-    private void OnDisable()
-    {
-        if (rayInteractor != null)
-        {
-            rayInteractor.selectEntered.RemoveListener(DisableLocomotions);
-            rayInteractor.selectExited.RemoveListener(EnableLocomotions);
-        }
+	private void Awake()
+	{
+		if (rayInteractor != null)
+			rayInteractor.gameObject.SetActive(true);
 
-        InputAction teleportModeActivate = this.teleportModeActivate?.action;
-        if (teleportModeActivate != null)
-        {
-            teleportModeActivate.performed -= OnStartTeleport;
-            teleportModeActivate.canceled -= OnStopTeleport;
-        }
-    }
+		if(teleportInteractor != null)
+			teleportInteractor.gameObject.SetActive(false);
 
-    private void EnableLocomotions(SelectExitEventArgs args)
-    {
-        foreach (LocomotionProvider locomotion in locomotions)
-        {
-            locomotion.gameObject.SetActive(true);
-        }
+	}
 
-        InputAction teleportModeActivate = this.teleportModeActivate?.action;
-        if (teleportModeActivate != null)
-            teleportModeActivate.Disable();
-    }
+	private void OnEnable()
+	{
+		if (rayInteractor != null)
+		{
+			rayInteractor.selectEntered.AddListener(OnRaySelectEnter);
+			rayInteractor.selectExited.AddListener(OnRaySelectExit);
+			rayInteractor.uiHoverEntered.AddListener(OnUIHoverEnter);
+			rayInteractor.uiHoverExited.AddListener(OnUIHoverExit);
+		}
 
-    private void DisableLocomotions(SelectEnterEventArgs args)
-    {
-        foreach (LocomotionProvider locomotion in locomotions)
-        {
-            locomotion.gameObject.SetActive(false);
-        }
+		if(teleportInput != null)
+		{
+			InputAction teleportAction = teleportInput.action;
+			teleportAction.performed += TeleportModeStart;
+			teleportAction.canceled += TeleportModeEnd;
+		}
+	}
 
-        InputAction teleportModeActivate = this.teleportModeActivate?.action;
-        if (teleportModeActivate != null)
-            teleportModeActivate.Enable();
-    }
+	private void OnDisable()
+	{
+		if (rayInteractor != null)
+		{
+			rayInteractor.selectEntered.RemoveListener(OnRaySelectEnter);
+			rayInteractor.selectExited.RemoveListener(OnRaySelectExit);
+			rayInteractor.uiHoverEntered.RemoveListener(OnUIHoverEnter);
+			rayInteractor.uiHoverExited.RemoveListener(OnUIHoverExit);
+		}
 
-    private void OnStartTeleport(CallbackContext context)
-    {
-        if (rayInteractor != null)
-            rayInteractor.gameObject.SetActive(false);
-        if (teleportInteractor != null)
-            teleportInteractor.gameObject.SetActive(true);
-    }
+		if (teleportInput != null)
+		{
+			InputAction teleportAction = teleportInput.action;
+			teleportAction.performed -= TeleportModeStart;
+			teleportAction.canceled -= TeleportModeEnd;
+		}
+	}
 
-    private void OnStopTeleport(CallbackContext context)
-    {
-        if (rayInteractor != null)
-            rayInteractor.gameObject.SetActive(true);
-        if (teleportInteractor != null)
-            StartCoroutine(TeleportDelay());
-    }
+	/// <summary>
+	/// Ray interactor로 무언가를 잡았을 때
+	/// </summary>
+	/// <param name="args"></param>
+	private void OnRaySelectEnter(SelectEnterEventArgs args)	
+	{
+		//ray interactor를 통해 오브젝트에 접근한 경우에는 locomotion을 못하도록 비활성 처리
+		foreach(var provider in locomotionProviders)
+		{
+			provider.gameObject.SetActive(false);
+		}
+	}
 
-    IEnumerator TeleportDelay()
-    {
-        yield return new WaitForEndOfFrame();   // Delay
-        teleportInteractor.gameObject.SetActive(false);
-    }
+	/// <summary>
+	/// ray interactor로 잡았던 것을 놨을 때
+	/// </summary>
+	/// <param name="args"></param>
+	private void OnRaySelectExit(SelectExitEventArgs args)
+	{
+		foreach (var provider in locomotionProviders)
+		{
+			provider.gameObject.SetActive(true);
+		}
+	}
+
+	private void OnUIHoverEnter(UIHoverEventArgs args)
+	{
+		foreach (var provider in locomotionProviders)
+		{
+			provider.gameObject.SetActive(true);
+		}
+	}
+
+	private void OnUIHoverExit(UIHoverEventArgs args)
+	{
+		foreach (var provider in locomotionProviders)
+		{
+			provider.gameObject.SetActive(false);
+		}
+	}
+
+	/// <summary>
+	/// teleport 키를 눌렀을 떄
+	/// </summary>
+	/// <param name="context"></param>
+	private void TeleportModeStart(InputAction.CallbackContext context)
+	{
+		//rayInteractor는 비활성 처리하고, teleport는 활성화 시킴
+
+		if (rayInteractor != null)
+			rayInteractor.gameObject.SetActive(false); 
+
+		if(teleportInteractor != null)
+			teleportInteractor.gameObject.SetActive(true);
+	}
+
+	private void TeleportModeEnd(InputAction.CallbackContext context)
+	{
+		if (rayInteractor != null)
+			rayInteractor.gameObject.SetActive(true);
+
+		if (teleportInteractor != null)
+			teleportInteractor.gameObject.SetActive(false);
+	}
 }
